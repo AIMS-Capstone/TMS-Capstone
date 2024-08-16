11d6ac9b744c35e618a5878719260e12fb6b5eb5
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2024-08-16 15:22:20 +0800

    First commit with laravel jetstream-livewire

---
+++ resources/views/components/authentication-card.blade.php
@@ -0,0 +1,11 @@
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 custom-background">
+  
+   
+
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="flex items-center sm:justify-center">
+        {{ $logo }}
+        </div>
+        {{ $slot }}
+    </div>
+</div>
