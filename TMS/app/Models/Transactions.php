7198e63834dae17c5979300672bd852bfd1a05f4
hatdogguldo <hatdogguldo@gmail.com>
2024-11-11 20:33:43 +0800

    fix: adjustments on models to access the added keys/foreign keys

--- TMS/app/Models/Transactions.php
+++ TMS/app/Models/Transactions.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
+
 
 class Transactions extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
-    protected $fillable = ['transaction_type', 'date', 'inv_number', 'reference', 'total_amount', 'contact', 'itr_include', 'total_amount_credit', 'total_amount_debit', 'vatable_sales','vatable_purchase', 'vat_amount', 'non_vatable_sales','non_vatable_purchase','status', 'organization_id' ];
+    use HasFactory, SoftDeletes;
+    protected $fillable = ['transaction_type', 'date', 'inv_number', 'reference', 'total_amount', 'contact', 'itr_include', 'total_amount_credit', 'total_amount_debit', 'vatable_sales','vatable_purchase', 'vat_amount', 'non_vatable_sales','non_vatable_purchase','status', 'organization_id', 'deleted_by' ];
 
     public function taxRows()
     {
@@ -31,6 +33,31 @@ class Transactions extends Model
 {
     return $this->hasMany(TaxReturnTransaction::class, 'transaction_id');
 }
+
+    //Organization name fetch
+    public function Organization()
+    {
+        return $this->belongsTo(Orgsetup::class, 'organization_id');
+    }
+
+    //display deleted by 
+    public function deletedByUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'deleted_by');
+    }
+
+     protected static function boot()
+    {
+        parent::boot();
+
+       static::deleting(function ($trasaction) {
+            if (!$$trasaction->isForceDeleting()) {
+                $trasaction->deleted_by = Auth::user()->id;
+                $trasaction->save();
+            }
+        });
+
+    }
 
 
 }
